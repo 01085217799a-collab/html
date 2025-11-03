22bfb0bb1a94d3595d9535005c97ab1f6dcf9146
01085217799a-collab <01085217799a@gmail.com>
2025-11-03 17:36:44 +0900

    Add files via upload

---
+++ User.tsx
@@ -0,0 +1,18 @@
+const User = ({
+  userObj,
+  clickHandler,
+}: {
+  userObj: { name: string; age: number }
+  clickHandler: () => void
+}) => {
+  return (
+    <>
+      <p>이름:{userObj.name}</p>
+      <p>나이:{userObj.age}</p>
+      <button onClick={clickHandler}>클릭</button>
+    </>
+  )
+}
+
+export default User
+// 객체또는 부해 할당
